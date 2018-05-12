9316bbea61748cc19f5965c4d3c86cb55f740702
Yish <mombuartworks@gmail.com>
2018-05-12 17:17:07 +0800

    add main core ajax

---
+++ app/Http/Controllers/Api/Embed.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\EmbedRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use \Embed\Embed as EmbedProvide;
+
+class Embed extends Controller
+{
+    public function __invoke(EmbedRequest $request)
+    {
+        $parsed = EmbedProvide::create($request->get('url'));
+
+        return response()->json([
+                'title' => $parsed->title,
+                'description' => $parsed->description,
+                'url' => $parsed->url,
+                'image' => $parsed->image,
+        ]);
+    }
+}
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.app')
 
+@section('style')
+    <style>
+        .hidden {
+            display: none;
+        }
+    </style>
+@endsection
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
@@ -15,7 +22,7 @@
                                 <label for="description" class="col-sm-4 col-form-label text-md-right">Description</label>
 
                                 <div class="col-md-6">
-                                    <input id="description" type="text" class="form-control{{ $errors->has('description') ? ' is-invalid' : '' }}" name="description" value="{{ old('description') }}" required autofocus>
+                                    <textarea class="form-control{{ $errors->has('description') ? ' is-invalid' : '' }}" name="description" id="description" cols="30" rows="10" autofocus>{{ old('description') }}</textarea>
 
                                     @if ($errors->has('description'))
                                         <span class="invalid-feedback">
@@ -23,6 +30,9 @@
                                     </span>
                                     @endif
                                 </div>
+                            </div>
+
+                            <div id="preview" class="form-group row hidden">
                             </div>
 
                             <div class="form-group row mb-0">
@@ -38,4 +48,61 @@
             </div>
         </div>
     </div>
+@endsection
+
+@section('script')
+    <script>
+        $("#description").keyup(function (e) {
+            let val = $(this).val();
+            if (val.match(/^http([s]?):\/\/.*/)) {
+                if (e.which == 13) {
+                    axios.post('/api/embed', {
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        url: val
+                    }).then(function (response) {
+                        $("#preview").html(`
+                        <label class="col-sm-4 col-form-label text-md-right">preview</label>
+                            <div class="col-md-6">
+                                <div class="card" style="width: 18rem;">
+                                    <img id="preview_image" class="card-img-top" src="${response.data.image}" alt="Card image cap">
+                                    <div class="card-body">
+                                        <p id="preview_body" class="card-text">${response.data.title}</p>
+                                    </div>
+                                </div>
+                            </div>`).removeClass("hidden");
+                    })
+                        .catch(function (error) {
+                            console.log(error);
+                        });
+                }
+            }
+        }).on('paste', function(e) {
+            var data = e.originalEvent.clipboardData.getData('Text');
+            if (data.match(/^http([s]?):\/\/.*/)) {
+                axios.post('/api/embed', {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    url: data
+                })
+                .then(function (response) {
+                    $("#preview").html(`
+                        <label class="col-sm-4 col-form-label text-md-right">preview</label>
+                            <div class="col-md-6">
+                                <div class="card" style="width: 18rem;">
+                                    <img id="preview_image" class="card-img-top" src="${response.data.image}" alt="Card image cap">
+                                    <div class="card-body">
+                                        <p id="preview_body" class="card-text">${response.data.title}</p>
+                                    </div>
+                                </div>
+                            </div>`).removeClass("hidden");
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            }
+        });
+    </script>
 @endsection
